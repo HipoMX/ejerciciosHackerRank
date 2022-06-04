a96b76b3fb1919a67226f66d61958f3ea57e3271
Eduardo Hipólito <eduardohipolitog@outlook.com>
2022-06-04 12:19:55 -0500

    Almacena nombres y calificaciones en otra lista

--- nestedLists.py
+++ nestedLists.py
@@ -1,4 +1,10 @@
 if __name__ == '__main__':
-    for _ in range(int(input())):
+    python_students = []
+    for i in range(int(input())):
         name = input()
         score = float(input())
+        arr = [name, score]
+        python_students.append(arr)
+    print(python_students)
+
+
