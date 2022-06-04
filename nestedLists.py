18c664b7c3fefb3e0ce83134fbda7431425e0484
Eduardo Hipólito <eduardohipolitog@outlook.com>
2022-06-04 14:59:44 -0500

    Ordena en base al índice dado

--- nestedLists.py
+++ nestedLists.py
@@ -6,5 +6,6 @@ if __name__ == '__main__':
         arr = [name, score]
         python_students.append(arr)
     print(python_students)
-
+    print("Sorted List A based on index 0: % s" % (sorted(python_students, key=lambda x: x[0])))
+    print("Sorted List A based on index 1: % s" % (sorted(python_students, key=lambda x: x[1])))
 
