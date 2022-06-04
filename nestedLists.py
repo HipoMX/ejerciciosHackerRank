48cb541283dab8da7183ed30c1f984a56ede413b
Eduardo Hipólito <eduardohipolitog@outlook.com>
2022-06-04 15:52:06 -0500

    Debo hacer que el ciclo elimine el número más grande

--- nestedLists.py
+++ nestedLists.py
@@ -1,11 +1,19 @@
 if __name__ == '__main__':
+    second_place_students = []
     python_students = []
     for i in range(int(input())):
         name = input()
         score = float(input())
         arr = [name, score]
         python_students.append(arr)
-    print(python_students)
-    print("Sorted List A based on index 0: % s" % (sorted(python_students, key=lambda x: x[0])))
-    print("Sorted List A based on index 1: % s" % (sorted(python_students, key=lambda x: x[1])))
+    python_students = sorted(python_students, key=lambda x: x[1], reverse=True)
+    second_place = python_students[0][1]
 
+    for name, score in python_students:
+        if score == second_place:
+            second_place_students.append(name)
+
+    second_place_students.sort()
+
+    for name in second_place_students:
+        print(name, end='\n')
