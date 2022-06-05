807cf189a7fcb4d867983216e8eb2685a81a5d3e
Eduardo Hipólito <eduardohipolitog@outlook.com>
2022-06-05 12:39:11 -0500

    Utilicé el set() para que eliminara los números repetidos y así poder seleccionar el segundo más bajo para después seperar los nombres en otra lista en caso de que se llegara a repetir

--- nestedLists.py
+++ nestedLists.py
@@ -1,16 +1,19 @@
 if __name__ == '__main__':
     second_place_students = []
     python_students = []
+    scores = set()
+
     for i in range(int(input())):
         name = input()
         score = float(input())
         arr = [name, score]
+        scores.add(score)
         python_students.append(arr)
-    python_students = sorted(python_students, key=lambda x: x[1], reverse=True)
-    second_place = python_students[0][1]
+
+    second_lowest = sorted(scores)[1]
 
     for name, score in python_students:
-        if score == second_place:
+        if score == second_lowest:
             second_place_students.append(name)
 
     second_place_students.sort()
